5d020c35f792950a7e9c10184a2c1b18e1932f1c
simsum <sven@Svens-MacBook-Pro.local>
2014-06-12 17:09:09 +0200

    Change Function Name

--- SetNixieClock.ino
+++ SetNixieClock.ino
@@ -53,7 +53,6 @@ void setup()
   unixtime = makeTime(tm); 
 
   // Daylight saving time activ (false = standard time, true = daylight saving time +1)
-  int DaylightSavingAtSetup = SommerzeitAktiv(tmYearToCalendar(tm.Year), tm.Month, tm.Day, tm.Hour, tm.Minute);
   // DaylightSaving adjustment, RTC should work with standard time
   if (DaylightSavingAtSetup) unixtime = unixtime - 3600;
 
